37118ece0bccf2fb5dd0562d88ee92241129ab90
user <athimbityana@gmail com>
2022-06-08 14:09:03 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        String province[] = new String [7];
+
+        province[0] = "G";
+        province[1] = "L";
+        province[2] = "M";
+        province[3] = "W";
+        province[4] = "E";
+        province[5] = "F";
+
+        /*
+        for(int r=0;r<province.length;r++)
+        {
+        System.out.println(province[r];
+        }
+        }
+         */
+
+        int r = 0;
+        do {
+            System.out.println(province[r]);
+            r++;
+        }
+        while (r<province.length);
+
+
+        }
+    }
